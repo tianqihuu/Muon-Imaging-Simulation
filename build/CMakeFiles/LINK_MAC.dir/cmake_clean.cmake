2ef8642d06fe0cfc0dd20c025d994859352680b8
John <john@Johns-MacBook-Pro.local>
2019-05-15 00:15:57 +0800

    Never upload build directory

--- build/CMakeFiles/LINK_MAC.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/LINK_MAC"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/LINK_MAC.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
